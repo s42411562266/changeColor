ebc285b0929f4fe50d0b3fccbcbcb0cadcd5a3dc
PUPU <s42411562266@gmail.com>
2019-03-14 18:55:52 +0800

    HW1(2019/3/14)

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,10 +13,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    int size=18;
+    int size=30;
+
     public void bigger(View w){
            TextView big;
            big=findViewById(R.id.textView2);
            big.setTextSize(++size);
+    }
+    public void smaller(View w){
+        TextView small=findViewById(R.id.textView2);
+        small.setTextSize(--size);
     }
 }
