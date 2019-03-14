4172a007f46605aff0fed911886ab41fa3b585c1
pupu <s42411562266@gmail.com>
2019-03-14 13:13:47 +0800

    HW1

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    int size=18;
+    public void bigger(View w){
+           TextView big;
+           big=findViewById(R.id.textView2);
+           big.setTextSize(++size);
     }
 }
